8948dec28bd3dc482f5e457fb29f3185f0884fb7
FraserProvan2 <FraserProvan2@gmail.com>
2020-04-11 19:04:48 +0100

    gets list of dinos

--- src/crawler.js
+++ src/crawler.js
@@ -1,24 +1,33 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const nhm_dict = "https://www.nhm.ac.uk/discover/dino-directory/name/name-az-all.html";
+const cheerio = require("cheerio");
+const utils = require("./helpers/utils");
+const nhm_dino_dict = "https://www.nhm.ac.uk/discover/dino-directory/name/name-az-all.html";
 
 module.exports = {
-  getRandomDino: () => {
+  getRandomDino () {
+    const dinos = getDinoDirectory();
+    console.log(dinos);
+  
+    // pick a dino
+
+    // format to response
+
     return {
       name: "**trex**\n big and scary",
-      img_src: "https://cdn.britannica.com/83/211283-131-3DB62136/Tyrannosaurus-Rex-Struthiomimus-dinosaurs.jpg"
-    }
+      img_src:
+      "https://cdn.britannica.com/83/211283-131-3DB62136/Tyrannosaurus-Rex-Struthiomimus-dinosaurs.jpg",
+    };
   },
-}
+};
 
 function getDinoDirectory() {
+  let html = utils.getHtml(nhm_dino_dict);
+  let $ = cheerio.load(html);
 
-  // let $ = cheerio.load(html);
-  // $('p.dinosaurfilter--name').each((index, element) => {
-  //   let dino_name = element.children[0].data.trim();
-  //   dino_dict.push(dino_name);
-  // });
+  let dino_dict = [];
+  $('p.dinosaurfilter--name').each((index, element) => {
+    let dino_name = element.children[0].data.trim();
+    dino_dict.push(dino_name);
+  });
 
-  // return dino_dict;
+  return dino_dict;
 }
-getDinoDirectory();
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,8 +1,14 @@
-
-const chalk = require('chalk');
+const chalk = require("chalk");
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 module.exports = {
   botSpeak: (message) => {
     console.log(chalk.redBright(message));
   },
-}
+  getHtml(url) {
+    let request = new XMLHttpRequest();
+    request.open('GET', url, false);  // `false` makes the request synchronous
+    request.send(null);
+    return request.responseText;
+  },
+};
